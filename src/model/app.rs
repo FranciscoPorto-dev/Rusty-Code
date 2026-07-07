3026b2e79691d31b3614586e5b0be3047ccd69c6
FranciscoPorto-dev <franmed1276@gmail.com>
2026-07-08 00:07:41 +0100

    fix: code that was previously commited had some minor mistakes due to not having an instance of the app inside of the model

--- src/model/app.rs
+++ src/model/app.rs
@@ -1,34 +1,45 @@
+use tokio::runtime::{Handle, Runtime};
+use tokio::sync::mpsc;
+
 pub struct App {
     pub input: String,
     pub character_index: usize,
     pub input_mode: InputMode,
     pub edit_history: Vec<(String, usize)>,
     pub messages: Vec<Message>,
+    pub is_streaming: bool,
+    api_tx: mpsc::Sender<ApiEvent>,
+    api_rx: mpsc::Receiver<ApiEvent>,
+    _runtime: Runtime,
+    runtime: Handle,
 }
 
 impl App {
     pub fn new() -> Self {
+        let runtime = Runtime::new().expect("failed to create Tokio runtime");
+        let runtime_handle = runtime.handle().clone();
+        let (api_tx, api_rx) = mpsc::channel(32);
+
         Self {
             messages: Vec::new(),
             input: String::new(),
             input_mode: InputMode::Normal,
             character_index: 0,
             edit_history: Vec::new(),
+            is_streaming: false,
+            api_tx,
+            api_rx,
+            _runtime: runtime,
+            runtime: runtime_handle,
         }
     }
 }
 
-pub num ApiEvent {
+pub enum ApiEvent {
     Delta(String),
     Done,
     Error(String),
 }
-
-
-pub is_streaming:bool,
-api_tx: mpsc::Sender<ApiEvent>,
-api_rx: mpsc::Receiver<ApiEvent>,
-runtime: tokio:runtime::Handle,
 
 pub enum Role {
     User,
